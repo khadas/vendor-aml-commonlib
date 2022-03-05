7d45dc157a76a6f6e0707ad593182e64216d8331
Jun Zhang <jun.zhang@amlogic.com>
2022-03-05 12:37:58 +0800

    yocto: fix pthread destr left in __pthread_keys after exit [1/1]
    PD#SWPL-73324
    
    Problem:
     - while IPC shutdown, pthread key not deleted, which will cause crash
       with dlopen method
    
    Solution:
     - pthread_delete_key while shutdown
    
    Verify:
    local
    
    Change-Id: I30c7806cd6a97ce7770a99825399d2699d780b2f
    Signed-off-by: Jun Zhang <jun.zhang@amlogic.com>

--- libbinder/binder/IPCThreadState.cpp
+++ libbinder/binder/IPCThreadState.cpp
@@ -326,6 +326,7 @@ void IPCThreadState::shutdown()
             delete st;
             pthread_setspecific(gTLS, NULL);
         }
+        pthread_key_delete(gTLS);
         gHaveTLS = false;
     }
 }
