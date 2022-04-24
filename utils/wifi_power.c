a3357c33f22426db32da83835ebac0faa2055b28
Jun Zhang <jun.zhang@amlogic.com>
2022-04-24 11:48:52 +0800

    wifi_power: fix coding style, add interface parameter [1/2]
    PD#TV-101553
    
    Problem:
    fixed `wlan0` interface in source code
    
    Solution:
    fix wifi power coding style
    add interface parameter, only check sys node if interface
    parameter is specified.
    
    Verify:
    t5w
    
    Change-Id: Id274448d5a515125f1721e5ce77c18dc44d81e60
    Signed-off-by: Jun Zhang <jun.zhang@amlogic.com>

--- utils/wifi_power.c
+++ utils/wifi_power.c
@@ -1,98 +1,94 @@
-#include <fcntl.h>
-#include <asm/ioctl.h>
-#include <stdlib.h>
-#include <stdio.h>
 #include <dirent.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
 
-#define USB_POWER_UP    _IO('m',1)
-#define USB_POWER_DOWN  _IO('m',2)
-#define SDIO_POWER_UP    _IO('m', 3)
-#define SDIO_POWER_DOWN  _IO('m', 4)
-#define SDIO_POWER_DOWN  _IO('m', 4)
-#define SDIO_GET_DEV_TYPE  _IO('m', 5)
+#define USB_POWER_UP _IO('m', 1)
+#define USB_POWER_DOWN _IO('m', 2)
+#define SDIO_POWER_UP _IO('m', 3)
+#define SDIO_POWER_DOWN _IO('m', 4)
+#define SDIO_POWER_DOWN _IO('m', 4)
+#define SDIO_GET_DEV_TYPE _IO('m', 5)
 
 char dev_type[128] = {0};
 
-void set_wifi_power(int on)
-{
-    int fd;
+bool set_wifi_power(int on) {
+  int fd;
+  bool ret = true;
 
-    fd = open("/dev/wifi_power", O_RDWR);
-    if (fd != -1) {
-        if (on == 0) {
-            if (ioctl(fd,SDIO_POWER_DOWN)<0) {
-                if (ioctl(fd,USB_POWER_DOWN) < 0) {
-                    printf("Set Wi-Fi power down error!!!\n");
-                    close(fd);
-                    return;
-                }
-            }
-        } else if (on == 1) {
-            if (ioctl(fd,SDIO_POWER_UP) < 0) {
-                if (ioctl(fd,USB_POWER_UP) < 0) {
-                    printf("Set Wi-Fi power up error!!!\n");
-                    close(fd);
-                    return;
-                }
-            }
-        } else if (on == 2) {
-            if (ioctl(fd, SDIO_GET_DEV_TYPE, dev_type) < 0) {
-                printf("Set Wi-Fi power up error!!!\n");
-                close(fd);
-                return;
-            } else
-                printf("inf=%s\n", dev_type);
-       }
-       close(fd);
-    } else {
-        printf("Device open failed !!!\n");
+  fd = open("/dev/wifi_power", O_RDWR);
+  if (fd < 0) {
+    fprintf(stderr, "Device open failed !!!\n");
+    return false;
+  }
+
+  switch (on) {
+  case 0: {
+    if (ioctl(fd, SDIO_POWER_DOWN) < 0) {
+      if (ioctl(fd, USB_POWER_DOWN) < 0) {
+        fprintf(stderr, "Set Wi-Fi power down error!!!\n");
+        close(fd);
+        ret = false;
+      }
     }
+  } break;
+  case 1: {
+    if (ioctl(fd, SDIO_POWER_UP) < 0) {
+      if (ioctl(fd, USB_POWER_UP) < 0) {
+        fprintf(stderr, "Set Wi-Fi power up error!!!\n");
+        close(fd);
+        ret = false;
+      }
+    }
+  } break;
+  case 2: {
+    if (ioctl(fd, SDIO_GET_DEV_TYPE, dev_type) < 0) {
+      fprintf(stderr, "Failed to get dev type!!!\n");
+      close(fd);
+      ret = false;
+    } else
+      fprintf(stdout, "inf=%s\n", dev_type);
+  } break;
+  default:
+    fprintf(stderr, "Invalid command %d\n", on);
+    ret = false;
+  }
+  close(fd);
 
-    return;
+  return ret;
 }
 
-int is_driver_loaded(const char *intface)
-{
-    DIR *d;
-    struct dirent *de;
+int main(int argc, char *argv[]) {
+  long value = 0;
+  if (argc < 2) {
+    fprintf(stderr, "wrong number of arguments\n");
+    return -1;
+  }
+  value = strtol(argv[1], NULL, 10);
+  if (!set_wifi_power(value)) {
+    return -1;
+  }
 
-    d = opendir("/sys/class/net");
-    if (d == 0) {
-       printf("fail to open /sys/class/net\n");
-       return 0;
-    }
-    while ((de = readdir(d))) {
-        if (strcmp(de->d_name, intface)== 0) {
-            closedir(d);
-            return 1;
-        }
-    }
-    closedir(d);
-    return 0;
-}
+  if (value == 1 && argc > 2) {
+    const char *interface_name = argv[2];
 
-int main(int argc, char *argv[])
-{
-    long value = 0;
-    if (argc != 2) {
-        printf("wrong number of arguments\n");
-        return -1;
+    int wait_time = 100;
+    char fpath[64];
+    snprintf(fpath, sizeof(fpath), "/sys/class/net/%s", interface_name);
+    do {
+      if (access(fpath, 0) == 0)
+        break;
+      usleep(50000);
+    } while (wait_time--);
+    if (wait_time < 0) {
+      fprintf(stderr, "wait for interface [%s] timeout\n", interface_name);
+      return -1;
     }
-    value = strtol(argv[1], NULL, 10);
-    set_wifi_power(value);
+  }
 
-    if (value == 1) {
-        int wait_time = 0;
-        do {
-            if (is_driver_loaded("wlan0"))
-                break;
-            else {
-                wait_time++;
-                usleep(50000);
-            }
-        } while (wait_time < 100);
-    }
-
-    return 0;
+  return 0;
 }
 
